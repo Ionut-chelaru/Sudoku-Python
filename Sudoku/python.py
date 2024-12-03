41fe4102abed8b6d55c40880fd0de1e9819da287
Ionut <ionut.chelaru2@student.tuiasi.ro>
2024-12-03 08:25:23 +0200

    schimbari nume functii

--- Sudoku/python.py
+++ Sudoku/python.py
@@ -31,100 +31,100 @@ class Sudoku(ctk.CTk):
 
         self.config(menu=self.menu_bar)
 
-        self.show_home_screen()
+        self.afisare_acasa()
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)    
         self.grid_rowconfigure(5, weight=1)
 
-    def clear_window(self):
+    def curata_ecran(self):
         for widget in self.winfo_children():
             widget.destroy()
 
 
     ######## Home screen ############
-    def show_home_screen(self):
-        self.clear_window()
+    def afisare_acasa(self):
+        self.curata_ecran()
         Welcome_text = ctk.CTkLabel(self,text='SUDOKU',font=('Century Gothic', 40))
         Welcome_text.grid(row=0, column=0, padx=0, pady=0)
 
         Credits = ctk.CTkLabel(self,text='Made by: Ionut Chelaru\nPersonal project',font=('Century Gothic', 10))
         Credits.grid(row=5, column=0, padx=0, pady=0)
 
-        menu_button = ctk.CTkButton(self, text="Enter game", command=self.show_menu,font=("Ariel",14,'bold'))
+        menu_button = ctk.CTkButton(self, text="Enter game", command=self.afisare_meniu,font=("Ariel",14,'bold'))
         menu_button.grid(row=1, column=0, padx=0, pady=0)
-        exist_game = ctk.CTkButton(self, text="Exit game", command=self.close_window,font=("Ariel",14,'bold'))
+        exist_game = ctk.CTkButton(self, text="Exit game", command=self.inchide_fereastra,font=("Ariel",14,'bold'))
         exist_game.grid(row=2, column=0, padx=0, pady=10)
     ########### Menu ################
-    def show_menu(self):
-        self.clear_window()
+    def afisare_meniu(self):
+        self.curata_ecran()
         Welcome_text = ctk.CTkLabel(self,text='SUDOKU',font=('Century Gothic', 40))
         Welcome_text.grid(row=0, column=0, padx=0, pady=0)
 
 
-        option1_button = ctk.CTkButton(self, text="Start new game", command=self.Start_game,font=("Ariel",14,'bold'))
+        option1_button = ctk.CTkButton(self, text="Start new game", command=self.Incepe_jocul,font=("Ariel",14,'bold'))
         option1_button.grid(row=1, column=0, padx=0, pady=0)
         option2_button = ctk.CTkButton(self, text="Options", command=self.Options,font=("Ariel",14,'bold'))
         option2_button.grid(row=2, column=0, padx=0, pady=10)
-        option3_button = ctk.CTkButton(self, text="Exit", command=self.close_window,font=("Ariel",14,'bold'))
+        option3_button = ctk.CTkButton(self, text="Exit", command=self.inchide_fereastra,font=("Ariel",14,'bold'))
         option3_button.grid(row=3, column=0, padx=0, pady=0)
 
-    def close_window(self):
+    def inchide_fereastra(self):
         self.destroy()
 
 
 
-    def Start_game(self):
-        self.clear_window()
+    def Incepe_jocul(self):
+        self.curata_ecran()
         label = ctk.CTkLabel(self, text="Choose difficuly", font=("Century Gothic", 26,'bold'))
         label.grid(row=0, column=0, padx=0, pady=0)
-        button2 = ctk.CTkButton(self, text="Easy", command=lambda: self.Dificulty(1),fg_color='#229954',text_color='#d0d3d4',height=40,hover_color='#145a32',font=("Ariel",14,'bold'))   
+        button2 = ctk.CTkButton(self, text="Easy", command=lambda: self.dificultate(1),fg_color='#229954',text_color='#d0d3d4',height=40,hover_color='#145a32',font=("Ariel",14,'bold'))   
         button2.grid(row=1, column=0, padx=0, pady=0)
-        button3 = ctk.CTkButton(self, text="Medium", command=lambda: self.Dificulty(2),fg_color='#b7950b',text_color='#d0d3d4',height=40,hover_color='#7d6608',font=("Ariel", 14,'bold'))
+        button3 = ctk.CTkButton(self, text="Medium", command=lambda: self.dificultate(2),fg_color='#b7950b',text_color='#d0d3d4',height=40,hover_color='#7d6608',font=("Ariel", 14,'bold'))
         button3.grid(row=2, column=0, padx=0, pady=(10,0))
-        button4 = ctk.CTkButton(self, text="Hard", command=lambda: self.Dificulty(3),fg_color='#b03a2e',text_color='#d0d3d4',height=40,hover_color='#641e16',font=("Ariel", 14,'bold'))
+        button4 = ctk.CTkButton(self, text="Hard", command=lambda: self.dificultate(3),fg_color='#b03a2e',text_color='#d0d3d4',height=40,hover_color='#641e16',font=("Ariel", 14,'bold'))
         button4.grid(row=3, column=0, padx=0, pady=10)
-        button1 = ctk.CTkButton(self, text="Back to Menu", command=self.show_menu,font=("Ariel", 14,'bold'))
+        button1 = ctk.CTkButton(self, text="Back to Menu", command=self.afisare_meniu,font=("Ariel", 14,'bold'))
         button1.grid(row=4, column=0, padx=0, pady=(30,0))
 
 
-    def Dificulty(self,value):
-        self.clear_window()
+    def dificultate(self,value):
+        self.curata_ecran()
         if value == 1:
             label = ctk.CTkLabel(self, text="Difficulty set to easy", font=("Century Gothic", 20,'bold'))
             label.grid(row=1, column=0, padx=0, pady=0)
-            self.after(3000, self.remove_label_and_create_grid, label)
+            self.after(3000, self.tranzitie_meniu_joc, label)
         if value == 2:
             label = ctk.CTkLabel(self, text="Difficulty set to medium", font=("Century Gothic", 20,'bold'))
             label.grid(row=1, column=0, padx=0, pady=0)
-            self.after(3000, self.remove_label_and_create_grid, label)
+            self.after(3000, self.tranzitie_meniu_joc, label)
         if value == 3:
             label = ctk.CTkLabel(self, text="Difficulty set to hard", font=("Century Gothic", 20,'bold'))
             label.grid(row=1, column=0, padx=0, pady=0)
-            self.after(3000, self.remove_label_and_create_grid, label)
+            self.after(3000, self.tranzitie_meniu_joc, label)
 
-    def remove_label_and_create_grid(self, label):
+    def tranzitie_meniu_joc(self, label):
 
         label.grid_forget()
 
-        self.create_grid()
+        self.creare_grila()
 
     def Options(self):
-        self.clear_window()
+        self.curata_ecran()
         label = ctk.CTkLabel(self, text="Change options", font=("Century Gothic", 20))
         label.grid(row=0, column=0, padx=0, pady=0)
         
-        button = ctk.CTkButton(self, text="Back to Menu", command=self.show_menu)
+        button = ctk.CTkButton(self, text="Back to Menu", command=self.afisare_meniu)
         button.grid(row=1, column=0, padx=0, pady=0)
 
-    def on_entry_change(self, entry):
+    def schimbare_stare_citire(self, entry):
         if entry.get() != "":  
             entry.config(state="readonly")  
     def is_digit(self, value):
         return value == "" or value.isdigit()        
 
-    def create_grid(self):
-        self.clear_window()
+    def creare_grila(self):
+        self.curata_ecran()
         self.config(bg='#00202e')
 
         grid_frame = tk.Frame(self,bg="#003f5c", bd=2, relief="solid")
@@ -151,7 +151,7 @@ class Sudoku(ctk.CTk):
                 validate_cmd = self.register(self.is_digit)
                 entry = ttk.Entry(grid_frame, width=2, justify='center', font=('Arial', 18),style='Custom.TEntry',validate="key", validatecommand=(validate_cmd, '%P'))
                 entry.bind("<FocusIn>", lambda event, entry=entry: entry.icursor(0))
-                entry.bind("<KeyRelease>", lambda event, entry=entry: self.on_entry_change(entry))
+                entry.bind("<KeyRelease>", lambda event, entry=entry: self.schimbare_stare_citire(entry))
                 entry.grid(
                     row=i,
                     column=j,
